2605f31f2806d9b7629392ed77a43d0b6318c569
BrandonVieira-C <brandon.c.vieira@gmail.com>
2022-04-15 17:24:02 -0400

    updated package.json

--- Frontend/backyardfrontend/server.js
+++ Frontend/backyardfrontend/server.js
@@ -7,6 +7,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/backyardfrontend'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/weddingappfrontendangular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/backyardfrontend/index.html'));
 });
 app.listen(process.env.PORT || 8080);
