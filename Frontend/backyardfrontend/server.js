19bd8e74f753aa971e27d65c4e959d9997e6d2db
BrandonVieira-C <brandon.c.vieira@gmail.com>
2022-04-15 17:17:49 -0400

    updated package.json

--- Frontend/backyardfrontend/server.js
+++ Frontend/backyardfrontend/server.js
@@ -1,15 +1,12 @@
-// Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files from the dist directory
-app.use(express.static('./dist/backyardfrontend'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/weddingappfrontendangular/'}),
-);
 
 // Start the app by listening on the dault heroku port
+app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/backyardfrontend'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/weddingappfrontendangular/index.html'));
+});
 app.listen(process.env.PORT || 8080);
